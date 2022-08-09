3b6ec091b533125a3c55b48e2a3e2ee672a3ec93
Ariful Islam <iariful.info@gmail.com>
2022-08-10 01:28:59 +0600

    try vitejs with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold" >Hello vite</h1>
+    </div>
+  )
+}
+
+export default App
